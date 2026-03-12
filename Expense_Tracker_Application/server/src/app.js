23baf54493e3f5b5f819b93fef3e9aa909419f34
Hoanglanh03 <hlanh.20082k3@gmail.com>
2026-03-12 23:17:22 +0700

    create swagger in project

--- Expense_Tracker_Application/server/src/app.js
+++ Expense_Tracker_Application/server/src/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
-
 require("dotenv").config();
+
+const setupSwagger = require("./setups/swaggerSetup");
+const expenseRouter = require("./routes/expenseRouter");
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const expenseRouter = require("./routes/expenseRouter");
+setupSwagger(app);
 
-app.use("/expense", expenseRouter);
-
+app.use("/expenses", expenseRouter);
 app.listen(PORT, () => {
   console.log(`App listening on port http://localhost:${PORT} `);
 });
