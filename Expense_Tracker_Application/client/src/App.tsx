6bc3ca00b2f4bf88df887b9a612713041251fda3
Hoanglanh03 <hlanh.20082k3@gmail.com>
2026-03-24 12:19:57 +0700

    init project

---
+++ Expense_Tracker_Application/client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <h1 className="bg-blue-500 text-white p-4">Expense Tracker Project</h1>
+      <p>Bắt đầu xây dựng giao diện ở đây...</p>
+    </div>
+  );
+}
+
+export default App;
